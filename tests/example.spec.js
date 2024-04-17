0461d24a1120c6c4f2a8d03a294c4a90eaad4f81
Mancer Abd-Elfetah <abdelfetahmancer@gmail.com>
2024-04-18 01:12:15 +0200

    changed

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,6 +8,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
   await page.goto('https://google.fr')
   await page.locator('//div[@class="hello"]')
+  await page.locator("")
 });
 
 test('get started link', async ({ page }) => {
